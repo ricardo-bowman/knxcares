b96a280b01ae7568356f19a8908beb7c8125f1df
Ken Cavagnolo <62471007+ricardo-bowman@users.noreply.github.com>
2020-03-25 08:33:07 -0400

    closing

--- data/config.js
+++ data/config.js
@@ -1,19 +1,19 @@
 module.exports = {
   // Name of your site. Can be the name of a brand, or your personal name.
-  title: 'Gatsby Starter',
+  title: 'KNX Cares',
   // What's the purpose of this website? What can we expect from it?
   description:
     `A portfolio based on your latest Instagram posts, implemented with Uber's Base Web Design System.`,
   // Your legal name.
-  legalName: 'Gatsby Starter',
+  legalName: 'KNX Cares',
   // URL to this website. If you bought `www.domain.com`, then it will be `www.domain.com`.
-  url: 'https://www.robonomy.com',
+  url: 'https://www.knxcares.org',
   // Favicon that will display on browsers.
   logo: 'static/images/favicon.png',
   // Who made this website?
-  author: 'Juan Alejandro Morais',
+  author: 'Ricardo Bowman',
   // What's the website of the author?
-  authorUrl: 'https://www.timrodz.com',
+  authorUrl: 'https://github.com/ricardo-bowman/knxcares',
   // Your social media presence, in the form of usernames.
   social: {
     // tells the Instagram API the username to get posts from. 
@@ -35,13 +35,13 @@ module.exports = {
   backgroundColor: '#fff',
   // No more information than City + Country due to privacy concerns.
   address: {
-    city: 'City',
-    country: 'Country',
+    city: 'Knoxville, TN',
+    country: 'USA',
   },
   // Basic contact details.
   contact: {
     // Email address to contact you.
-    email: 'timrodz@icloud.com',
+    email: 'ricardobowman194@gmail.com',
   },
   // When was this website created?
   foundingDate: '2020',
